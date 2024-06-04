74849425ea45b1785aacaf555eefac07b50550db
stef-ania <stebofof@gmail.com>
2024-06-04 12:53:44 +0200

    Restructured html tags in WeatherInfo

--- src/app/Components/WeatherInfo/WeatherInfo.jsx
+++ src/app/Components/WeatherInfo/WeatherInfo.jsx
@@ -1,19 +1,23 @@
 import React from "react";
 import styles from "./WeatherInfo.module.css";
 import WeatherIcon from "../WeatherIcon/WeatherIcon";
-import dateUtils from "../../utils/dateUtils";
 
 export default function WeatherInfo({ city, temperature, description, humidity, wind, day, time, icon }) {
   return (
     <div className={styles.weatherInfoWrapper}>
-      <h3>{city}</h3>
-      <p>{temperature}°C</p>
-      <p>Description: {description}</p>
-      <p>Humidity: {humidity}%</p>
-      <p>Wind: {wind}km/h</p>
-      <p>Day: {day}</p>
-      <p>Time: {time}</p>
-      <WeatherIcon code={icon} alt={description} />
+      <div className={styles.weatherMainInfo}>
+        <h1>{city}</h1>
+        <p>
+          {day} {time}, {description}
+        </p>
+        <p>
+          Humidity: <strong className={styles.weatherAccent}>{humidity}%</strong>, Wind:{" "}
+          <strong className={styles.weatherAccent}>{wind}km/h</strong>
+        </p>
+      </div>
+      <div className={styles.weatherMainTemperature}>
+        <WeatherIcon code={icon} alt={description} /> <span>{temperature}°C</span>
+      </div>
     </div>
   );
 }
